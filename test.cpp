6611f089ebaeb869d6397e56b442472af24154e6
ABiR994 <salemnurabir2@gmail.com>
2025-08-19 18:09:33 +0600

    test.cpp file updated in test_branch.

--- test.cpp
+++ test.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-
+    int bin, dec = 0;
+    cin >> bin;
+    int i = 0;
+    while(bin != 0) {
+        int rem = bin % 10;
+        dec += rem << i;
+        bin /= 10;
+        i++;
+    }
+    cout << dec << endl;
     return 0;
 }
