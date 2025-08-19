7b716b3d3fb4c2ed511c4f4bbb8003a8e3e4ee1b
ABiR994 <salemnurabir2@gmail.com>
2025-08-19 18:20:19 +0600

    binary_to_decimal.cpp file in Codes folder updated.

--- Codes/binary_to_decimal.cpp
+++ Codes/binary_to_decimal.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main() {
-    int bin, dec = 0, pow = 1;
+    int bin, dec = 0;
+    //int  pow = 1;
+    int pow = 0;
     cout << "Enter the binary number: ";
     cin >> bin;
     while(bin > 0) {
         int rem = bin % 10;
-        dec += rem * pow;
         bin /= 10;
-        pow *= 2;
+    //     dec += rem * pow;
+        dec += rem << pow;
+    //     pow *= 2;
+        pow++;
     }
     cout << "\nDecimal Value: " << dec << endl;
     return 0;
