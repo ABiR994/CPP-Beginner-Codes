f404794af94688ba2daea20cc9dc0a1c11eadeb7
ABiR994 <salemnurabir2@gmail.com>
2025-08-24 13:25:12 +0600

    test.cpp file updated in test_branch.

--- test.cpp
+++ test.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <string.h>
+#include <cstring>
 using namespace std;
 
 int main() {
-    int bin, dec = 0;
-    cin >> bin;
-    int i = 0;
-    while(bin != 0) {
-        int rem = bin % 10;
-        dec += rem << i;
-        bin /= 10;
-        i++;
-    }
-    cout << dec << endl;
+    char s1[] = "Hello, ";
+    char s2[] = "World!";
+    cout << strlen(s1) << endl;
+    cout << strlen(s2) << endl;
+    char s3[] = "Hello, ";
+    strcat(s3, s2);
+    cout << s3 << endl;
+    cout << strcmp(s1, s2) << endl;
+    cout << strcmp(s2, s1) << endl;
+    cout << strcmp(s1, s1) << endl;
+    cout << strcmp(s2, s3) << endl;
+    strcpy(s3, s1);
+    cout << s3 << endl;
+    strncat(s3, s1, 3);
+    cout << s3 << endl;
+    cout << strncmp(s1, s2, 3) << endl;
+    cout << strncmp(s2, s1, 3) << endl;
+    cout << strncmp(s1, s1, 3) << endl;
+    cout << strncmp(s2, s3, 3) << endl;
+    strncpy(s3, s1, 3);
+    cout << s3 << endl;
+    cout << strchr(s1, 'H') << endl;
+    cout << strchr(s1, 'o') << endl;
+    cout << strchr(s1, 'l') << endl;
+    char a[] = "Hello, World!";
+    char b[] = "World";   cout << strstr(a, b) << endl;
     return 0;
 }
