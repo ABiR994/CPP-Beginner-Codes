58272420f9d056d34b7e5c61aee9f23d26dadf52
ABiR994 <salemnurabir2@gmail.com>
2025-08-25 19:13:54 +0600

    test.cpp file updated in test_branch.

--- test.cpp
+++ test.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int linear_search(int arr[], int size, int target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i;
+void intersection(int arr1[], int size1, int arr2[], int size2) {
+    for(int i = 0; i < size1; i++) {
+        for(int j = 0; j < size2; j++) {
+            if(arr1[i] != arr2[j]) {
+                continue;
+            }
+            else {
+                cout << arr1[i] << " ";
+            }
         }
     }
-    return -1;
 }
 
 int main() {
-    int arr[] = {4, 2, 7, 8, 1, 2, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target;
-    cout << "Enter the target value to search: ";
-    cin >> target;
-    cout << "Index of target (" << target << "): " << linear_search(arr, size, target) << endl;
+    int arr1[] = {1, 4, 3, 2, 5, 7, 6};
+    int arr2[] = {9, 2, 1, 3, 6, 8};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    intersection(arr1, size1, arr2, size2);
+    cout << endl;
     return 0;
 }
