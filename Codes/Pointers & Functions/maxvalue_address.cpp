677cce212d1e64909fbb363a37e9e338535d86c3
ABiR994 <salemnurabir2@gmail.com>
2025-09-12 21:54:24 +0600

    maxvalue_address.cpp file added.

---
+++ Codes/Pointers & Functions/maxvalue_address.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int* findMax(int *arr, int size) {
+    int *maxAdd = arr;
+    for (int i = 1; i < size; i++) {
+        if (*(arr + i) > *maxAdd) {
+            maxAdd = arr + i;
+        }
+    }
+    return maxAdd;
+}
+
+int main() {
+    int arr[] = {10, -2, 33, 14, 45, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int *maxAdd = findMax(arr, size);
+
+    cout << "Max Value: " << *maxAdd << endl;
+    cout << "Max Address: " << maxAdd << endl;
+
+    return 0;
+}
