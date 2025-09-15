4b3ebc53377c67cc56328e36f8199459e6ac7c98
ABiR994 <salemnurabir2@gmail.com>
2025-09-15 18:54:52 +0600

    linear_search.cpp file updated.

--- Codes/2D Array/linear_search.cpp
+++ Codes/2D Array/linear_search.cpp
@@ -3,19 +3,30 @@ using namespace std;
 
 
 int main() {
-    int arr[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    int target = 8;
-    for(int i =0; i < 3; i++) {
+    int arr[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    int target;
+    int r = 0, c = 0;
+    bool found = false;
+
+    cout << "Enter the element to search: ";
+    cin >> target;
+
+    for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
             if(arr[i][j] == target) {
-                cout << "Found at (" << i << ", " << j << ")" << endl;
+                r = i;
+                c = j;
+                found = true;
                 break;
             }
         }
     }
+
+    if(found) {
+        cout << "Element found at position: (" << r << ", " << c << ")" << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
     return 0;
 }
